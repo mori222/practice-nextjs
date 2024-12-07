269cb602a1f2718d0af176b85dd1294c2ec5fa94
mori222 <h.mori@bpx.co.jp>
2024-12-08 04:56:04 +0900

    ✨ 説明用useEffectの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
-import InteractiveButton from '../components/button'
-
+import { useEffect } from 'react';
 import Footer from "../components/footer";
-import Main from "../components/main";
 import Header from "../components/header";
+import InteractiveButton from '../components/button';
+import Main from "../components/main";
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = 'lightblue';
+
+    return () => {
+      document.body.style.backgroundColor = '';
+    }
+  }, [])
+
   return (
-    <div className="grid grid-rows-[100px_1fr_100px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[100px_80px_1fr_100px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <Header />
       <InteractiveButton onClick={() => alert('ボタンが押されました！')}>
         ここをクリック！
