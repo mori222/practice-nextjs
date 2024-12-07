f6615a60ba32e69ddb113156fc24c30d9c1a155d
mori222 <h.mori@bpx.co.jp>
2024-12-08 05:19:20 +0900

    ✨ カウンターを設置してuseStateでカウントアップ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,32 @@
 'use client'
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Footer from "../components/footer";
 import Header from "../components/header";
 import InteractiveButton from '../components/button';
 import Main from "../components/main";
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+  
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
 
     return () => {
       document.body.style.backgroundColor = '';
     }
-  }, [])
+  }, []);
 
   return (
-    <div className="grid grid-rows-[100px_80px_1fr_100px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[100px_50px_50px_1fr_100px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <InteractiveButton onClick={() => alert('ボタンが押されました！')}>
+      <h1>{count}</h1>
+      <InteractiveButton onClick={handleClick}>
         ここをクリック！
       </InteractiveButton>
       <Main page="index"/>
