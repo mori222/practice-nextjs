7d039926ab0d7189d9f450929899e10ec836efe1
mori222 <h.mori@bpx.co.jp>
2024-12-08 07:10:58 +0900

    ✨ useStateについて文字列と真偽値を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import Main from "../components/main";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
   
   const handleClick = useCallback(
     () => {
@@ -26,13 +28,33 @@ export default function Home() {
     }
   }, []);
 
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 10) {
+      alert('10文字以上入力しないでください');
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[100px_50px_50px_1fr_100px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <InteractiveButton onClick={handleClick}>
         ここをクリック！
       </InteractiveButton>
+      <button onClick={handleDisplay}>
+        {isShow ? '非表示' : '表示'}
+      </button>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={handleChange}
+      />
       <Main page="index"/>
       <Footer />
     </div>
