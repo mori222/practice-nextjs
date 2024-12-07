c30ff863d7be1f079f33d66629c1ce3c9f2b611f
mori222 <h.mori@bpx.co.jp>
2024-12-08 07:43:24 +0900

    ♻️ 元の状態にprevに名前変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const handleClick = useCallback(
     () => {
       if (count < 10) {
-        setCount((count) => count + 1);
+        setCount((prevCount) => prevCount + 1);
       }
     },
     [count]
@@ -37,7 +37,7 @@ export default function Home() {
   }, []);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   return (
